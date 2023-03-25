34908ad69256cfac0ba591222788de8ed46e9c74
dennistaiti <dennistaiti7@gmail.com>
2023-03-25 09:43:01 +0300

    completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,18 @@
-class MoviesController < ApplicationController
-  wrap_parameters false
+class Movie < ApplicationRecord
   
-  def index
-    movies = Movie.all
-    render json: movies
-  end
+  CATEGORIES = ['Comedy', 'Drama', 'Animation', 'Mystery', 'Horror', 'Fantasy', 'Action', 'Documentary', 'Science Fiction']
 
-  def create
-    movie = Movie.create(movie_params)
-    render json: movie, status: :created
-  end
+  validates :title, presence: true
+  validates :year, numericality: {
+    greater_than_or_equal_to: 1888,
+    less_than_or_equal_to: Date.today.year
+  }
+  validates :poster_url, presence: true
+  validates :category, inclusion: {
+    in: CATEGORIES,
+    message: "must be one of: #{CATEGORIES.join(', ')}"
+  }
 
-  private
 
-  def movie_params
-    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
-  end
-  
 end
+
